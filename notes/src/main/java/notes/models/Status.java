207d79a62c5bbe15f563db741dc1d0eb32f6bddd
Dmitry Ivashnev <85080252+iIvDmAl@users.noreply.github.com>
2023-10-31 12:52:16 +0300

    Revert "Support PR"

--- notes/src/main/java/notes/models/Status.java
+++
@@ -1,9 +0,0 @@
-package notes.models;
-
-public enum Status {
-    NULL_SIZE, LIST_MENU, LIST_UPDATE, NEW, CLOSED;
-
-    public static void printNullSize(){
-        System.out.println("Заметка с таким именем не существует.");
-    }
-}
