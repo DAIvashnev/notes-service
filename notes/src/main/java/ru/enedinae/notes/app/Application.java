02e585b42e376fca7fb6c63091a6074919786306
enedinae <ida94b@mail.ru>
2023-12-12 20:35:32 +0300

    Проект на Spring Core

--- notes/src/main/java/ru/enedinae/notes/app/Application.java
+++ notes/src/main/java/ru/enedinae/notes/app/Application.java
@@ -1,10 +1,6 @@
 package ru.enedinae.notes.app;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.stereotype.Component;
-import ru.enedinae.notes.db.DataBaseManager;
-import ru.enedinae.notes.mapper.NoteMapper;
-import ru.enedinae.notes.repository.impl.NoteRepositoryImpl;
 import ru.enedinae.notes.service.impl.CheckDeadline;
 import ru.enedinae.notes.ui.impl.CommandLineUiImpl;
 
